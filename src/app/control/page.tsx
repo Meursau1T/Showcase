3cdf025985eb3bb1e2466385958a41159b0cf885
MeursaulT <meursaultw@outlook.com>
2025-06-06 13:27:21 +0800

    添加控制页守卫

--- src/app/control/page.tsx
+++ src/app/control/page.tsx
@@ -1,3 +1,15 @@
-export default async function ControlPage() {
+import { PageParam } from '@/type';
+import { cookies } from 'next/headers';
+import { redirect } from "next/navigation";
+import { parseLang, getSession } from '@/utils';
+
+export default async function ControlPage({ searchParams }: PageParam) {
+  const lang = await parseLang(searchParams);
+  const session = await getSession(cookies);
+
+  if (!session.isLoggedIn) {
+    redirect('/')
+  }
+
   return <div>{'Control'}</div>;
 }
